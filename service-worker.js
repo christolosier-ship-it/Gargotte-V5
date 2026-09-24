01fa04efb04bd3e535b2160ce64988df2ff252ae
Kristo <christo.losier@gmail.com>
2026-09-24 08:08:19 +0200

    Scope Service Worker cleanup to Gargottex caches only

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
 
 const CACHE = "gargottex-v6-rembg-retired-v1";
+const CACHE_PREFIX = "gargottex-";
 const ASSETS = [
   "./",
   "./index.html",
@@ -84,7 +85,7 @@ self.addEventListener("install", event => {
 self.addEventListener("activate", event => {
   event.waitUntil((async () => {
     const keys = await caches.keys();
-    await Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)));
+    await Promise.all(keys.filter(k => k !== CACHE && k.startsWith(CACHE_PREFIX)).map(k => caches.delete(k)));
     await self.clients.claim();
   })());
 });
